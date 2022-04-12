d61fc99a647c3ab64017b9d9380fe6684da0abbc
youngsun0401 <youngsun0401@naver.com>
2022-04-12 16:41:24 +0900

    Create Car.java

---
+++ mysqlConnection/Car.java
@@ -0,0 +1,19 @@
+package dto;
+
+public class Car {
+	private int num;
+	private String ctype;
+	private int cyear;
+	
+	//// constructor using all fields
+	public Car(int num, String ctype, int cyear) {
+		this.num = num;
+		this.ctype = ctype;
+		this.cyear = cyear;
+	}
+	
+	//// getters
+	public int getNum() {return num;}
+	public String getCtype() {return ctype;}
+	public int getCyear() {return cyear;}
+}
